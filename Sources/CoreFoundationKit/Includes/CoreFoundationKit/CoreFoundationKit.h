329a73eb18eb80596bcfacfefa5f3b471f8e0757
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-07-26 17:57:31 +0800

    Set: Initial implementation

--- Sources/CoreFoundationKit/Includes/CoreFoundationKit/CoreFoundationKit.h
+++ Sources/CoreFoundationKit/Includes/CoreFoundationKit/CoreFoundationKit.h
@@ -4,17 +4,14 @@
  *
  *  Created by Fang Ling on 2026/4/25.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  See the License for the specific language governing permissions and limitations under the License.
  */
 
 #ifndef CoreFoundationKit_h
@@ -27,6 +24,7 @@
 #include "../../Collections/CoreFoundationMutableArray.h"
 #include "../../Collections/CoreFoundationMutableDictionary.h"
 #include "../../Collections/CoreFoundationRedBlackTree.h"
+#include "../../Collections/CoreFoundationSet.h"
 
 #include "../../Geometry/CoreFoundationGeometry.h"
 
